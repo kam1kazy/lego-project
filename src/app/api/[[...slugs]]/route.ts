a603186925d98332fd0fc1ec64c23dbafaff4d54
kam1kazy <t2nda@yandex.ru>
2024-09-10 17:46:44 +0300

    причесал файлик сервера

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -4,16 +4,18 @@ import { cors } from '@elysiajs/cors'
 import { swagger } from '@elysiajs/swagger'
 
 // GRAPHQL
-import { schema } from '@graphql/schema'
 import { apollo } from '@elysiajs/apollo'
+import { schema } from '@graphql/schema'
 
 const app = new Elysia({ prefix: '/api' })
   .use(cors())
-  .use(swagger())
   .use(apollo(schema))
-  .get('/test', () => 'get test - ok')
+  .use(swagger())
 
 export type App = typeof app
 
 export const GET = app.handle
 export const POST = app.handle
+export const PUT = app.handle
+export const PATCH = app.handle
+export const DELETE = app.handle
